6213e1c763334f42c6bea2b1239295108c8b0d6a
Malinosky, Kyle Macon <kylperry@indiana.edu>
2021-04-13 02:28:32 -0400

    Delete n413connect.php

--- n413connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-        $dbhost = 'localhost'; 
-        $dbuser = 'kylperry';
-        $dbpwd  = 'whisks farrowing sontag'; 
-        $dbname = 'kylperry_3_db';
-        $link = mysqli_connect($dbhost, $dbuser, $dbpwd, $dbname);
-        if (!$link) { die('Connect Error (' . mysqli_connect_errno() . ') '. mysqli_connect_error()); }
-    ?>
